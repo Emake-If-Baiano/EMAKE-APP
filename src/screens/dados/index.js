2f212956f6799746b827e857d99106901247170e
Paulo Almeida <paulo1.valadares@gmail.com>
2023-05-05 22:05:00 -0300

    Update!

---
+++ src/screens/dados/index.js
@@ -0,0 +1,304 @@
+import React, { useEffect, useState } from 'react'
+
+import Background from '../../components/secondBackgrund';
+import { Text } from 'react-native';
+import { View } from 'react-native';
+import { Image } from 'react-native';
+
+import { OnboardFlow, PrimaryButton } from 'react-native-onboard';
+
+import TextInput from '../../components/TextInput';
+
+import Logo from '../../components/Logo';
+
+import Button from '../../components/Button';
+
+import Header from '../../components/Header';
+
+const b = PrimaryButton({ currentPage: 0, totalPages: 3, text: "oi" });
+
+function button(...data) {
+    data[0].text = data[0].text === "Continue" ? "Continuar" : "Iniciar!";
+    data[0].style = {
+        backgroundColor: "rgb(4, 252, 92)",
+    };
+
+    console.log(data[0])
+    return PrimaryButton(...data)
+};
+
+import Login from '../../services/SUAP';
+
+import { Dimensions } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { TouchableOpacity } from 'react-native';
+
+export default function Notificações({ navigation }) {
+
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        AsyncStorage.getItem("userdata").then(data => {
+            setUserData(JSON.parse(data));
+        });
+
+        console.log(userData)
+    }, [])
+
+    return (
+        <Background navigation={navigation}>
+            <View style={{
+                flex: 1,
+                justifyContent: "flex-end",
+                alignItems: "center",
+                alignSelf: "center",
+                width: Dimensions.get("window").width,
+                height: Dimensions.get("window").height,
+            }}>
+                <View style={{
+                    flex: 0.05,
+                }}>
+                    <TouchableOpacity style={{
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                        alignItems: "flex-start",
+                        flex: 1,
+                    }}>
+                        <Header customStyle={{
+                            color: "#00FF29",
+                            width: "100%",
+                            marginStart: "5%"
+                        }}>Sair </Header>
+                    </TouchableOpacity>
+                </View>
+                <View style={{
+                    flex: 0.9,
+                    backgroundColor: "white",
+                    width: "100%",
+                }}>
+                    <Header style={{
+                        flex: 0.05,
+                        color: "#225D62",
+                        fontSize: 20,
+                        alignSelf: "center",
+                    }}>
+                        Dados Gerais
+                    </Header>
+
+                    <View style={{
+                        flex: 0.1,
+                        flexDirection: "row",
+                        marginTop: "5%"
+                    }}>
+                        <Image
+                            style={{
+                                width: 50,
+                                height: 50,
+                                alignSelf: "center",
+                                borderRadius: 40,
+                                marginStart: "3%"
+                            }}
+                            source={{
+                                uri: "https://suap.ifbaiano.edu.br/" + userData.url_foto_75x100
+                            }}
+                        />
+
+                        <Header adjustsFontSizeToFit={true} customStyle={{
+                            color: "#225D62",
+                            fontSize: 17,
+                            alignSelf: "center",
+                            maxWidth: "80%",
+                            marginStart: "3%"
+                        }}>
+                            {userData?.vinculo?.nome}
+                        </Header>
+                    </View>
+
+                    <View style={{
+                        flex: 0.1,
+                        marginTop: "5%"
+                    }}>
+                        <View style={{
+                            flex: 0.4,
+                            backgroundColor: "#00FF29",
+                            width: "40%",
+                            borderTopEndRadius: 25,
+                            borderBottomEndRadius: 25,
+                        }}>
+                            <Header customStyle={{
+                                fontSize: 15,
+                                color: "#225D62",
+                                alignSelf: "center",
+                                fontWeight: "bold"
+                            }}>
+                                Email Acadêmico
+                            </Header>
+                        </View>
+
+                        <Header customStyle={{
+                            fontSize: 15,
+                            color: "#225D62",
+                            alignSelf: "center",
+                            flex: 0.6,
+                            fontWeight: "bold"
+                        }}>
+                            {userData.email}
+                        </Header>
+                    </View>
+
+                    <View style={{
+                        flex: 0.1,
+                        paddinTop: "10%"
+                    }}>
+                        <View style={{
+                            flex: 0.4,
+                            backgroundColor: "#00FF29",
+                            width: "40%",
+                            borderTopEndRadius: 25,
+                            borderBottomEndRadius: 25,
+                        }}>
+                            <Header customStyle={{
+                                fontSize: 15,
+                                color: "#225D62",
+                                alignSelf: "center",
+                                fontWeight: "bold"
+                            }}>
+                                CPF
+                            </Header>
+                        </View>
+
+                        <Header customStyle={{
+                            fontSize: 15,
+                            color: "#225D62",
+                            alignSelf: "center",
+                            flex: 0.6,
+                            fontWeight: "bold"
+                        }}>
+                            {userData.cpf}
+                        </Header>
+                    </View>
+
+                    <View style={{
+                        flex: 0.1,
+                        paddinTop: "10%"
+                    }}>
+                        <View style={{
+                            flex: 0.4,
+                            backgroundColor: "#00FF29",
+                            width: "40%",
+                            borderTopEndRadius: 25,
+                            borderBottomEndRadius: 25,
+                        }}>
+                            <Header customStyle={{
+                                fontSize: 15,
+                                color: "#225D62",
+                                alignSelf: "center",
+                                fontWeight: "bold"
+                            }}>
+                                Situação Sistêmica
+                            </Header>
+                        </View>
+
+                        <Header customStyle={{
+                            fontSize: 15,
+                            color: "#225D62",
+                            alignSelf: "center",
+                            flex: 0.6,
+                            fontWeight: "bold"
+                        }}>
+                            {userData.vinculo?.situacao_sistemica}
+                        </Header>
+                    </View>
+
+                    <View style={{
+                        flex: 0.1,
+                        paddinTop: "10%"
+                    }}>
+                        <View style={{
+                            flex: 0.4,
+                            backgroundColor: "#00FF29",
+                            width: "40%",
+                            borderTopEndRadius: 25,
+                            borderBottomEndRadius: 25,
+                        }}>
+                            <Header customStyle={{
+                                fontSize: 15,
+                                color: "#225D62",
+                                alignSelf: "center",
+                                fontWeight: "bold"
+                            }}>
+                                Situação Sistêmica
+                            </Header>
+                        </View>
+
+                        <Header customStyle={{
+                            fontSize: 15,
+                            color: "#225D62",
+                            alignSelf: "center",
+                            flex: 0.6,
+                            fontWeight: "bold"
+                        }}>
+                            {userData.vinculo?.situacao_sistemica}
+                        </Header>
+                    </View>
+                </View>
+            </View>
+        </Background>
+    )
+};
+
+const styles = {
+    logo_container: {
+        width: "100%",
+        height: 190,
+    },
+    logo_image: {
+        width: "100%",
+        height: 210,
+    },
+    pageStyleBoard: {
+        borderRadius: 25,
+        width: "100%",
+        height: "100%",
+    },
+    logo: {
+        flex: 0.05,
+    },
+    logo_if_container: {
+        flex: 0.15,
+        width: 110,
+        height: 110,
+    },
+    logo_if_image: {
+        width: 110,
+        height: 110,
+    },
+    logo_text_container: {
+        flex: 0.1,
+        maxWidth: "80%",
+    },
+    logo_text_text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        color: "white"
+    },
+    image_container: {
+        flex: 0.7,
+        alignItems: 'center',
+        borderRadius: 35,
+    },
+    image_container_2: {
+        flex: 1,
+        alignItems: 'center',
+        borderRadius: 35,
+    },
+    image_container_box: {
+        flex: 1,
+        borderRadius: 25,
+        width: "93%",
+        margin: 9
+    }
+}
---
+++ src/screens/perfil/index.js
@@ -0,0 +1,234 @@
+import React, { useEffect, useState } from 'react'
+
+import Background from '../../components/secondBackgrund';
+import { Text } from 'react-native';
+import { View } from 'react-native';
+import { Image } from 'react-native';
+
+import { OnboardFlow, PrimaryButton } from 'react-native-onboard';
+
+import TextInput from '../../components/TextInput';
+
+import Logo from '../../components/Logo';
+
+import Button from '../../components/Button';
+
+import Header from '../../components/Header';
+
+const b = PrimaryButton({ currentPage: 0, totalPages: 3, text: "oi" });
+
+function button(...data) {
+    data[0].text = data[0].text === "Continue" ? "Continuar" : "Iniciar!";
+    data[0].style = {
+        backgroundColor: "rgb(4, 252, 92)",
+    };
+
+    console.log(data[0])
+    return PrimaryButton(...data)
+};
+
+import Login from '../../services/SUAP';
+
+import { Dimensions } from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { TouchableOpacity } from 'react-native';
+
+export default function Notificações({ navigation }) {
+
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        AsyncStorage.getItem("userdata").then(data => {
+            setUserData(JSON.parse(data));
+        });
+
+        console.log(userData)
+    }, []);
+
+    if (!userData) return null;
+
+    return (
+        <Background navigation={navigation}>
+            <View style={{
+                flex: 1,
+                justifyContent: "flex-end",
+                alignItems: "center",
+                alignSelf: "center",
+                width: Dimensions.get("window").width,
+                height: Dimensions.get("window").height,
+            }}>
+                <View style={{
+                    flex: 0.05,
+                }}>
+                    <TouchableOpacity style={{
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                        alignItems: "flex-start",
+                        flex: 1,
+                    }}>
+                        <Header customStyle={{
+                            color: "#00FF29",
+                            width: "100%",
+                            marginStart: "5%"
+                        }}>Sair</Header>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{
+                    flex: 0.9,
+                    backgroundColor: "white",
+                    width: "100%",
+                }}>
+                    <View style={{
+                        flex: 0.05
+                    }}>
+
+                    </View>
+                    <View style={{
+                        flex: 0.3,
+                        width: "100%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}>
+                        <Image
+                            style={{
+                                width: 100,
+                                height: 100,
+                                alignSelf: "center",
+                                borderRadius: 50,
+                                marginStart: "3%",
+                            }}
+                            source={{
+                                uri: "https://suap.ifbaiano.edu.br/" + userData.url_foto_75x100
+                            }}
+                        />
+
+                        <Header customStyle={{
+                            color: "#225D62",
+                            fontSize: 20,
+                            maxWidth: "100%",
+                            textAlign: "center",
+                            alignSelf: "center",
+                            fontWeight: "bold"
+                        }}>
+                            {userData.vinculo?.nome}
+                        </Header>
+
+                        <Header selectable={true} customStyle={{
+                            marginTop: "1%",
+                            color: "#225D62",
+                            fontSize: 14,
+                            maxWidth: "70%",
+                            textAlign: "center",
+                            alignSelf: "center",
+                        }}>
+                            {userData.matricula}
+                        </Header>
+                    </View>
+
+                    <View style={{
+                        flex: 0.7,
+                        justifyContent: "space-evenly",
+                        alignItems: "center",
+                    }}>
+                        {[{
+                            name: "Meus Dados",
+                            image: require("../../../assets/meus_dados.png"),
+                            color: "rgb(196, 212, 236)",
+                            touch: () => navigation.navigate("Dados")
+                        }, {
+                            name: "Documentos",
+                            image: require("../../../assets/documentos.png"),
+                            color: "rgb(204, 220, 220)",
+                            touch: () => navigation.navigate("Perfil")
+                        }].map((category, index) => {
+                            return (
+                                <TouchableOpacity key={index} style={{
+                                    flexDirection: "row",
+                                    backgroundColor: category.color,
+                                    width: "100%",
+                                    flex: 0.2,
+                                    opacity: 1,
+                                    justifyContent: "space-evenly",
+                                    alignItems: "center",
+                                }} onPress={category.touch}>
+                                    <Image
+                                        style={{
+                                            width: 50,
+                                            height: 50,
+                                            alignSelf: "center",
+                                        }}
+                                        source={category.image}
+                                    />
+
+                                    <Header customStyle={{
+                                        color: "#225D62",
+                                        fontSize: 20,
+                                        opacity: 1
+                                    }}>
+                                        {category.name}
+                                    </Header>
+                                </TouchableOpacity>)
+                        })}
+                    </View>
+                </View>
+            </View>
+        </Background>
+    )
+};
+
+const styles = {
+    logo_container: {
+        width: "100%",
+        height: 190,
+    },
+    logo_image: {
+        width: "100%",
+        height: 210,
+    },
+    pageStyleBoard: {
+        borderRadius: 25,
+        width: "100%",
+        height: "100%",
+    },
+    logo: {
+        flex: 0.05,
+    },
+    logo_if_container: {
+        flex: 0.15,
+        width: 110,
+        height: 110,
+    },
+    logo_if_image: {
+        width: 110,
+        height: 110,
+    },
+    logo_text_container: {
+        flex: 0.1,
+        maxWidth: "80%",
+    },
+    logo_text_text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        color: "white"
+    },
+    image_container: {
+        flex: 0.7,
+        alignItems: 'center',
+        borderRadius: 35,
+    },
+    image_container_2: {
+        flex: 1,
+        alignItems: 'center',
+        borderRadius: 35,
+    },
+    image_container_box: {
+        flex: 1,
+        borderRadius: 25,
+        width: "93%",
+        margin: 9
+    }
+}
